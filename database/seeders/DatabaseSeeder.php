fdedfe24d43a40ca7699b8574c5fd9f789a04db9
chris-bhaila <chrisbhaila5@gmail.com>
2026-05-15 10:18:45 +0545

    Booking Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PropertySettingSeeder::class,
+            InventorySeederLocation2::class,
+            InventorySeederLocation3::class,
         ]);
 
         // User::factory(10)->create();
